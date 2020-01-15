017c1c0752ec6fcefe2b97caffbe47765b41da39
Sebastian Stampfel <sebastian.stampfel@gmail.com>
2020-01-15 09:57:42 +0100

    Some changes

--- cmake-build-debug/CMakeFiles/gdi_algorithmen.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/gdi_algorithmen.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/sebastian/CLionProjects/gdi-algorithmen/main.c" "/home/sebastian/CLionProjects/gdi-algorithmen/cmake-build-debug/CMakeFiles/gdi_algorithmen.dir/main.c.o"
+  "/home/mint-box/CLionProjects/bic1-gdi-algorithmen/main.c" "/home/mint-box/CLionProjects/bic1-gdi-algorithmen/cmake-build-debug/CMakeFiles/gdi_algorithmen.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- main.c
+++ main.c
@@ -8,16 +8,22 @@ struct node {
     char text[20];
 };
 
-int main() {
-    long long int amount = 10000000;
-    struct node* nodeArray = malloc(sizeof(struct node) * amount);
-    srand(time(0));
+struct node* randomizedArray(int elementCount){
+    struct node* array = malloc(sizeof(struct node) * elementCount);
 
-    for(int i = 0; i < amount; i++){
-        struct node* current = nodeArray + i;
+    for(int i = 0; i < elementCount; i++){
+        struct node* current = array + i;
         current->number = rand() % 32767;
         strcpy(current->text, "Hello World!");
     }
+
+    return array;
+}
+
+int main() {
+    int amount = 1000;
+    struct node* nodeArray = randomizedArray(amount);
+    srand(time(0));
 
     for(int i = 0; i < amount; i++){
         struct node* current = nodeArray + i;
